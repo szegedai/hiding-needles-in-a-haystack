2455348f279c3dedf833f656cf27929ef81768d5
arppy <berta@inf.u-szeged.hu>
2021-08-05 15:36:22 +0200

    minorminorminor debug

--- deep_backdoor.py
+++ deep_backdoor.py
@@ -369,10 +369,10 @@ def train_model(net1, net2, train_loader, train_scope, num_epochs, loss_mode, be
       # Prints mini-batch losses
       if loss_mode == "onlydetectorloss" :
         print('Training: Batch {0}/{1}. Loss of {2:.5f}'.format(
-              idx + 1, len(train_loader), train_loss.data,) end='')
+              idx + 1, len(train_loader), train_loss.data), end='')
       else :
         print('Training: Batch {0}/{1}. Loss of {2:.5f}, injection loss of {3:.5f}, detect loss of {4:.5f},'.format(
-              idx + 1, len(train_loader), train_loss.data, loss_generator.data, loss_detector.data,) end='')
+              idx + 1, len(train_loader), train_loss.data, loss_generator.data, loss_detector.data), end='')
       print(' backdoor l2 min: {0:.3f}, avg: {1:.3f}, max: {2:.3f}, backdoor linf'
             ' min: {3:.3f}, avg: {4:.3f}, max: {5:.3f}'.format(
         torch.min(l2).item(), torch.mean(l2).item(), torch.max(l2).item(),
