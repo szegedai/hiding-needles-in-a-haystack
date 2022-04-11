fd6bfa2b472704dedba6a2301d898ecc092eddb7
arppy <berta@inf.u-szeged.hu>
2022-04-11 13:31:37 +0200

    calmp once

--- deep_backdoor.py
+++ deep_backdoor.py
@@ -540,14 +540,13 @@ def maximazing_input(backdoor_generator_model, backdoor_detect_model, loader, nu
       valid_images.requires_grad = True
       optimizer.zero_grad()
       output = backdoor_model(valid_images)
-      print(idx,epoch,torch.mean(output).item(),torch.min(output).item(),torch.sum(output>0.0).item())
+      print(idx,epoch,torch.mean(output).item(),torch.min(output).item(),torch.sum(output<=0.0).item())
       torch.sum(-output).backward()
       optimizer.step()
-      valid_images.requires_grad = False
-      valid_images = torch.clamp(valid_images, 0.0, 1.0)
+    valid_images = torch.clamp(valid_images, 0.0, 1.0)
     output = backdoor_model(valid_images)
-    num_of_problem_images += torch.sum(output>0.0).item()
-    print(idx,"after clamp",torch.mean(output).item(),torch.min(output).item(),torch.sum(output>0.0).item(),num_of_problem_images)
+    num_of_problem_images += torch.sum(output<=0.0).item()
+    print(idx,"after clamp",torch.mean(output).item(),torch.min(output).item(),torch.sum(output<=0.0).item(),num_of_problem_images)
 
 def train_model(net1, net2, train_loader, batch_size, valid_loader, train_scope, num_epochs, loss_mode, alpha, beta, l, l_step, linf_epsilon_clip, l2_epsilon_clip, reg_start, learning_rate, device, pos_weight, jpeg_q):
   # Save optimizer
